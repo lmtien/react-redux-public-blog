e41d19bd365612d06bd7854b36713df48468154d
Tien Le <tiduslmt@gmail.com>
2017-05-31 18:15:26 +0800

    add deleting a post

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 export const FETCH_POST = 'FETCH_POST';
 export const FETCH_POSTS = 'FETCH_POSTS';
 export const CREATE_POST = 'CREATE_POST';
+export const DELETE_POST = 'DELETE_POST';
 
 const ROOT_URL = 'https://reduxblog.herokuapp.com/api';
 const API_KEY = '?key=abc';
@@ -32,5 +33,15 @@ export function fetchPost(id) {
   return {
     type: FETCH_POST,
     payload: request
+  };
+}
+
+export function deletePost(id, callback) {
+  const request = axios.delete(`${ROOT_URL}/posts/${id}${API_KEY}`)
+                       .then(() => callback());
+  
+  return {
+    type: DELETE_POST,
+    payload: id
   };
 }
--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,8 +1,11 @@
 import _ from 'lodash';
-import { FETCH_POSTS, FETCH_POST } from '../actions';
+import { FETCH_POSTS, FETCH_POST, DELETE_POST } from '../actions';
 
 export default function(state = {}, action) {
   switch (action.type) {
+    case DELETE_POST:
+      return _.omit(state, action.payload);
+      
     case FETCH_POST:
       // const post = action.payload.data;
       // const newState =  { ...state };
