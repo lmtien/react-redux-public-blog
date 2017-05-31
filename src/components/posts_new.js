56516b318c98d9c534e4dd8655909cdd2aa5bb8f
Tien Le <tiduslmt@gmail.com>
2017-05-31 11:19:56 +0800

    navigation after created post

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -27,7 +27,9 @@ class PostsNew extends Component {
   }
   
   onSubmit(values) {
-    this.props.createPost(values);
+    this.props.createPost(values, () => {
+      this.props.history.push('/');
+    });
   }
   
   render() {
